65910cf89e0ad93fee2682ff2798d8b21f52975b
Elia Perantoni <perantonielia0@gmail.com>
2022-07-29 20:25:00 +0200

    Fixed silly error

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -7,12 +7,10 @@
 // TODO Add test
 TorrentFile::TorrentFile(const bencode::BencodeValue& tree) {
   // We know the torrent starts with a dict
-  auto dict = dynamic_cast<const bencode::BencodeDict&>(tree).value();
+  auto& dict = dynamic_cast<const bencode::BencodeDict&>(tree).value();
 
-  std::unique_ptr<bencode::BencodeValue>& announce_entry =
-      (dict.at("announce"));
   this->announce_url =
-      dynamic_cast<bencode::BencodeString&>(*announce_entry).value();
+      dynamic_cast<bencode::BencodeString&>(*dict.at("announce")).value();
 
   // TODO Finish
 }
