c4626d1bf37e8c20df6d64ed1b44854a23485088
Elia Perantoni <perantonielia0@gmail.com>
2022-07-29 17:15:35 +0200

    Fix comment style

--- src/peer.hpp
+++ src/peer.hpp
@@ -2,23 +2,20 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
-/**
- * Contains data structures and facilities for representing and discovering
- * BitTorrent peers
- */
+#include "torrent.hpp"
+
+/// Contains data structures and facilities for representing and discovering
+/// BitTorrent peers
 namespace peer {
-/**
- * Represents a single peer as given by the tracker
- */
+/// Represents a single peer as given by the tracker
 struct Peer {
   uint32_t ip;
   uint16_t port;
 
-  /**
-   * Combines the ip and port of the peer into a X.Y.Z.W:PORT string
-   * @return The combined string
-   */
+  /// Combines the ip and port of the peer into a X.Y.Z.W:PORT string
+  /// \return The combined string
   [[nodiscard]] std::string address() const;
 };
 
