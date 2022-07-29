06b894300acd60b28c3777608f220eece1ab76b0
Elia Perantoni <perantonielia0@gmail.com>
2022-07-29 17:34:21 +0200

    Slightly improved peer address testing

--- test/peer.cpp
+++ test/peer.cpp
@@ -2,7 +2,12 @@
 
 #include "peer.hpp"
 
-TEST_CASE("Peer's address formatting") {
+TEST_CASE("[Peer] Address formatting") {
+  peer::Peer peer {1 << 24 | 2 << 16 | 3 << 8 | 4, 6565};
+  REQUIRE(peer.address() == "1.2.3.4:6565");
+}
+
+TEST_CASE("[Peer] Peer's address formatting") {
   peer::Peer peer {1 << 24 | 2 << 16 | 3 << 8 | 4, 6565};
   REQUIRE(peer.address() == "1.2.3.4:6565");
 }
