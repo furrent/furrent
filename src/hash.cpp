a1116838acb6b61f9f062f4248ab628de9f72527
Elia Perantoni <perantonielia0@gmail.com>
2022-07-30 14:34:01 +0200

    Hash use "std::array" instead of C array. Add piece hashes splitting

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,9 +1,36 @@
 #include "hash.hpp"
 
+#include <array>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
 std::string hash_to_str(const hash_t& hash) {
-  // "static_cast" cannot be used because the compiler cannot infer that
-  // "char" is the same as "unsigned char" even though we forced that with a
-  // flag. Must use "reinterpret_cast". The second parameter lets us include
-  // any null bytes.
-  return std::string{reinterpret_cast<const char*>(hash), 20};
+  return std::string{hash.begin(), hash.end()};
+}
+
+std::vector<hash_t> split_piece_hashes(const std::string& piece_hashes_str) {
+  if (piece_hashes_str.length() % 20 > 0) {
+    throw std::invalid_argument("malformed piece hashes string");
+  }
+
+  std::vector<hash_t> result;
+  // Forced to use unsigned because that's what "std::string::length" returns
+  // even though signed integers are generally the better idea for iterator
+  // variables
+  for (unsigned i = 0; i < piece_hashes_str.length(); i += 20) {
+    hash_t this_hash;
+    std::fill(this_hash.begin(), this_hash.end(), 0);
+
+    // "std::string" a vector of "char", not "unsigned char" like our "hash_t"
+    // is. But we know we're forcing the compiler to make "char" unsigned so
+    // this is actually safe. We just need to convert the type of "hash_t" from
+    // an array of "unsigned char" to an array of "char" and we do that with
+    // "reinterpret_cast"
+    piece_hashes_str.copy(
+        reinterpret_cast<std::array<char, 20>::iterator>(this_hash.begin()), 20,
+        i);
+    result.push_back(this_hash);
+  }
+  return result;
 }
--- src/hash.hpp
+++ src/hash.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <string>
+#include <vector>
 
 /// Type for a SHA1 hash
-using hash_t = uint8_t[20];
+using hash_t = std::array<uint8_t, 20>;
 
+/// Encodes an hash to a string. Note that we're forcing the compiler to make
+/// "char" unsigned so a string can contain arbitrary bytes
 std::string hash_to_str(const hash_t& hash);
+
+/// Takes a string with the hashes of pieces from a torrent file and parses
+/// them into a vector of "hash_t". Each hash is 20 bytes long
+std::vector<hash_t> split_piece_hashes(const std::string& pieces);
