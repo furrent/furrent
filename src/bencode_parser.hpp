4f0f386bd7b643654146f91d0e6bb52277905a1c
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2022-08-01 16:45:53 +0200

    Fix namespace identation

--- src/bencode_parser.hpp
+++ src/bencode_parser.hpp
@@ -9,15 +9,15 @@
 
 #include "bencode_value.hpp"
 
-namespace fur {
-  namespace bencode {
-    class BencodeParser {
-     public:
-      BencodeParser() = default;
-      ~BencodeParser() = default;
-      std::unique_ptr<BencodeValue> decode(std::string const &decoded);
-      std::string encode(BencodeValue const &value);
-    };
-  } // namespace bencode
-} // namespace fur
+namespace fur::bencode {
+
+class BencodeParser {
+ public:
+  BencodeParser() = default;
+  ~BencodeParser() = default;
+  std::unique_ptr<BencodeValue> decode(std::string const &decoded);
+  std::string encode(BencodeValue const &value);
+};
+
+}
 
--- src/bencode_value.hpp
+++ src/bencode_value.hpp
@@ -9,62 +9,62 @@
 #include <string>
 #include <vector>
 
-namespace fur {
-  namespace bencode {
-    enum class BencodeType { Integer, String, List, Dict };
+namespace fur::bencode{
 
-    class BencodeValue {
-     public:
-      virtual std::string to_string() const = 0;
-      // value function with generic return type
-      virtual BencodeType get_type() const = 0;
-    };
+enum class BencodeType { Integer, String, List, Dict };
 
-    class BencodeInt : public BencodeValue {
-     private:
-      int val;
+class BencodeValue {
+ public:
+  virtual std::string to_string() const = 0;
+  // value function with generic return type
+  virtual BencodeType get_type() const = 0;
+};
 
-     public:
-      explicit BencodeInt(int data);
-      virtual std::string to_string() const override;
-      virtual BencodeType get_type() const override;
-      [[nodiscard]] int value();
-    };
+class BencodeInt : public BencodeValue {
+ private:
+  int val;
 
-    class BencodeString : public BencodeValue {
-     private:
-      std::string val;
+ public:
+  explicit BencodeInt(int data);
+  virtual std::string to_string() const override;
+  virtual BencodeType get_type() const override;
+  [[nodiscard]] int value();
+};
 
-     public:
-      explicit BencodeString(std::string data);
-      virtual std::string to_string() const override;
-      virtual BencodeType get_type() const override;
-      [[nodiscard]] std::string& value();
-    };
+class BencodeString : public BencodeValue {
+ private:
+  std::string val;
 
-    class BencodeList : public BencodeValue {
-     private:
-      std::vector<std::unique_ptr<BencodeValue>> list;
+ public:
+  explicit BencodeString(std::string data);
+  virtual std::string to_string() const override;
+  virtual BencodeType get_type() const override;
+  [[nodiscard]] std::string& value();
+};
 
-     public:
-      explicit BencodeList(std::vector<std::unique_ptr<BencodeValue>> data);
-      virtual std::string to_string() const override;
-      virtual BencodeType get_type() const override;
-      [[nodiscard]] std::unique_ptr<std::vector<std::unique_ptr<BencodeValue>>>&
-      value();
-    };
+class BencodeList : public BencodeValue {
+ private:
+  std::vector<std::unique_ptr<BencodeValue>> list;
 
-    class BencodeDict : public BencodeValue {
-     private:
-      std::map<std::string, std::unique_ptr<BencodeValue>> dict;
+ public:
+  explicit BencodeList(std::vector<std::unique_ptr<BencodeValue>> data);
+  virtual std::string to_string() const override;
+  virtual BencodeType get_type() const override;
+  [[nodiscard]] std::unique_ptr<std::vector<std::unique_ptr<BencodeValue>>>&
+  value();
+};
 
-     public:
-      explicit BencodeDict(
-          std::map<std::string, std::unique_ptr<BencodeValue>> data);
-      virtual std::string to_string() const override;
-      virtual BencodeType get_type() const override;
-      [[nodiscard]] std::map<std::string, std::unique_ptr<BencodeValue>>&
-      value();
-    };
-  }  // namespace bencode
-}  // namespace fur
+class BencodeDict : public BencodeValue {
+ private:
+  std::map<std::string, std::unique_ptr<BencodeValue>> dict;
+
+ public:
+  explicit BencodeDict(
+      std::map<std::string, std::unique_ptr<BencodeValue>> data);
+  virtual std::string to_string() const override;
+  virtual BencodeType get_type() const override;
+  [[nodiscard]] std::map<std::string, std::unique_ptr<BencodeValue>>&
+  value();
+};
+
+}
