de9d334011a51c8ecc7c8de6861f2d56bff5a545
Elia Perantoni <perantonielia0@gmail.com>
2022-08-23 18:52:27 +0200

    Pool fix moving pointers bug

--- src/download/lender_pool.hpp
+++ src/download/lender_pool.hpp
@@ -1,57 +1,70 @@
 #include <condition_variable>
 #include <functional>
+#include <memory>
 #include <mutex>
+#include <optional>
 #include <vector>
 
 template <typename T>
-class LenderPool {
+struct Slot {
+  explicit Slot(T inner) : inner{std::move(inner)}, is_borrowed{false} {}
+
+  T inner;
+  bool is_borrowed;
+};
+
+template <typename T>
+using SlotPtr = std::unique_ptr<Slot<T>>;
+
+template <typename T>
+struct Borrow {
+  T& operator*() { return inner; }
+  T* operator->() { return &inner; }
+
+  explicit Borrow(T& inner, std::function<void()> onRelease)
+      : inner{inner}, onRelease{std::move(onRelease)} {}
+
+  ~Borrow() { onRelease(); }
+
  private:
-  struct Slot {
-    explicit Slot(T inner) : inner{std::move(inner)}, is_borrowed{false} {}
+  T& inner;
+  std::function<void()> onRelease;
+};
 
-    T inner;
-    bool is_borrowed;
-  };
+template <typename T>
+struct StratFirstAvailable {
+  SlotPtr<T>* operator()(std::vector<SlotPtr<T>>& storage);
+};
 
- public:
-  struct Borrow {
-    T& operator*() { return inner; }
-    T* operator->() { return &inner; }
-
-    explicit Borrow(T& inner, std::function<void()> onRelease)
-        : inner{inner}, onRelease{std::move(onRelease)} {}
-
-    ~Borrow() { onRelease(); }
-
-   private:
-    T& inner;
-    std::function<void()> onRelease;
-  };
-
+template <typename T, typename Strategy = StratFirstAvailable<T>>
+class LenderPool {
  private:
   std::condition_variable cv;
   std::mutex mx;
-  std::vector<Slot> storage;
+  std::vector<SlotPtr<T>> storage;
 
  public:
   void put(T inner) {
     std::unique_lock lock(mx);
-    storage.push_back(Slot(std::move(inner)));
+    storage.push_back(std::make_unique<Slot<T>>(std::move(inner)));
   }
 
-  Borrow get() {
+  Borrow<T> get() {
+    Strategy strategy;
+
     std::unique_lock lock(mx);
 
     while (true) {
-      for (auto& slot : storage) {
-        if (!slot.is_borrowed) {
-          slot.is_borrowed = true;
-          return Borrow(slot.inner, [&] {
-            std::unique_lock lock(mx);
-            slot.is_borrowed = false;
-            cv.notify_one();
-          });
-        }
+      SlotPtr<T>* maybeSlot = strategy(storage);
+
+      if (maybeSlot != nullptr) {
+        auto& slot = *(*maybeSlot).get();
+        slot.is_borrowed = true;
+        return Borrow(slot.inner, [&] {
+          std::unique_lock lock(mx);
+          slot.is_borrowed = false;
+          cv.notify_one();
+        });
       }
 
       // Too bad, there is no slot available to lend. Let's wait until something
@@ -60,3 +73,14 @@ class LenderPool {
     }
   }
 };
+
+template <typename T>
+SlotPtr<T>* StratFirstAvailable<T>::operator()(
+    std::vector<SlotPtr<T>>& storage) {
+  for (auto& slotPtr : storage) {
+    if (!slotPtr->is_borrowed) {
+      return &slotPtr;
+    }
+  }
+  return nullptr;
+}
--- test/lender_pool.cpp
+++ test/lender_pool.cpp
@@ -27,13 +27,42 @@ TEST_CASE("[LenderPool] Basic") {
 
   auto t1 = std::thread([&] {
     auto borrow = pool.get();
+    REQUIRE(borrow->proof_of_access() == 27);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     REQUIRE(borrow->proof_of_access() == 27);
   });
   auto t2 = std::thread([&] {
     auto borrow = pool.get();
+    REQUIRE(borrow->proof_of_access() == 27);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     REQUIRE(borrow->proof_of_access() == 27);
+  });
+
+  t1.join();
+  t2.join();
+}
+
+TEST_CASE("[LenderPool] Moving backing vector") {
+  // This test case demonstrates that the destruction of a borrow keeps working
+  // even after the contents of the pool are moved due to the backing vector
+  // being reallocated.
+
+  LenderPool<NonCopyable> pool;
+  pool.put(NonCopyable(27));
+  pool.put(NonCopyable(39));
+
+  auto t1 = std::thread([&] {
+    auto borrow = pool.get();
+    REQUIRE(borrow->proof_of_access() == 27);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    REQUIRE(borrow->proof_of_access() == 27);
+  });
+  auto t2 = std::thread([&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    auto borrow1 = pool.get();
+    auto borrow2 = pool.get();
+    REQUIRE(borrow1->proof_of_access() == 27);
+    REQUIRE(borrow2->proof_of_access() == 39);
   });
 
   t1.join();
