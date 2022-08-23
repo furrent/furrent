48e1731de634b61900e24db7b2d06715133d5efe
Elia Perantoni <perantonielia0@gmail.com>
2022-08-23 18:52:42 +0200

    Custom lender pool strategy

--- test/lender_pool.cpp
+++ test/lender_pool.cpp
@@ -68,3 +68,40 @@ TEST_CASE("[LenderPool] Moving backing vector") {
   t1.join();
   t2.join();
 }
+
+struct CustomStrategy {
+  using T = NonCopyable;
+  SlotPtr<T>* operator()(std::vector<SlotPtr<T>>& storage) {
+    // Even numbers first, then ascending order
+    for (auto& e : storage) {
+      if (e->is_borrowed) continue;
+      if (e->inner.proof_of_access() == 2) return &e;
+    }
+    for (auto& e : storage) {
+      if (e->is_borrowed) continue;
+      if (e->inner.proof_of_access() == 1) return &e;
+    }
+    for (auto& e : storage) {
+      if (e->is_borrowed) continue;
+      if (e->inner.proof_of_access() == 3) return &e;
+    }
+  }
+};
+
+TEST_CASE("[LenderPool] Custom strategy") {
+  LenderPool<NonCopyable, CustomStrategy> pool;
+  pool.put(NonCopyable(1));
+  pool.put(NonCopyable(2));
+  pool.put(NonCopyable(3));
+
+  auto t = std::thread([&] {
+    auto borrow1 = pool.get();
+    auto borrow2 = pool.get();
+    auto borrow3 = pool.get();
+    REQUIRE(borrow1->proof_of_access() == 2);
+    REQUIRE(borrow2->proof_of_access() == 1);
+    REQUIRE(borrow3->proof_of_access() == 3);
+  });
+
+  t.join();
+}
