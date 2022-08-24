2e019442991371293f132fc86e534435bf16eeb7
Elia Perantoni <perantonielia0@gmail.com>
2022-08-24 19:57:53 +0200

    Commenting

--- src/download/downloader.hpp
+++ src/download/downloader.hpp
@@ -10,11 +10,14 @@
 using namespace fur::peer;
 using namespace fur::torrent;
 
+/// The `TorrentFile` is not required because any given `Downloader` is already
+/// bound to a specific `TorrentFile` and has a reference to it.
 // TODO Remove once the real struct is merged
 struct Task {
   int index;
 };
 
+/// A downloaded piece for a torrent file.
 // TODO Remove once the real struct is merged
 struct Result {
   int index;
@@ -33,6 +36,13 @@ class Downloader {
   /// Construct a new `Downloader`. No TCP socket is established at this time.
   explicit Downloader(const TorrentFile& torrent, const Peer& peer);
 
+  /// Attempt downloading a piece using this `Downloader`. The function tries
+  /// it best not to throw any exception (unless something truly exceptional
+  /// happens). You can assume that any ordinary error will result in a
+  /// `std::nullopt` being returned. Errors such as:
+  ///  - This peer not having the requested piece available
+  ///  - The connection timing out
+  ///  - The downloaded piece being corrupt
   [[nodiscard]] std::optional<Result> try_download(const Task&);
 
  private:
