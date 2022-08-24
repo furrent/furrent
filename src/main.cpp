16380fe5201b6696fc6f569627113efae775b121
Filippo Ziche <filippo.ziche@gmail.com>
2022-08-24 17:08:58 +0200

    Cleared main

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,6 @@
 #include "furrent.hpp"
 
 int main() {
-  fur::Furrent f{};
-  f.add_torrent("../extra/debian-11.4.0-amd64-netinst.iso.torrent");
-  f.print_status();
-  //auto t = f.pick_torrent();
-  //if(t.has_tasks()){
-  //  auto task = t.pick_task();
-  //  std::cout << "Task: " << task.index << std::endl;
-  //  std::cout << t.has_tasks() << std::endl;
-  //}
-
-
 
   return 0;
 }
