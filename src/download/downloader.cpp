cbdf6aa4930c8d2b639add406cef398a7623851d
Elia Perantoni <perantonielia0@gmail.com>
2022-08-24 18:18:26 +0200

    Commenting `Downloader`

--- src/download/downloader.cpp
+++ src/download/downloader.cpp
@@ -18,6 +18,8 @@ void Downloader::ensure_connected() {
   auto port = asio::ip::port_type(peer.port);
   sock.connect(asio::ip::tcp::endpoint(ip, port));
 
+  // sock.set_option(asio::detail::socket_option::integer<SOL_SOCKET, SO_RCVTIMEO>(200));
+
   socket = std::move(sock);
 
   handshake();
@@ -28,11 +30,17 @@ void Downloader::ensure_connected() {
 // 8  for the extensions bits
 // 20 for the info hash
 // 20 for the peer id
+/// Length (in bytes) of a BitTorrent handshake.
 const int HANDSHAKE_LENGTH = 1 + 19 + 8 + 20 + 20;
+/// Offset (in bytes) from the beginning of a BitTorrent handshake for the
+/// info-hash field.
 const int INFO_HASH_OFFSET = 1 + 19 + 8;
 
 void Downloader::handshake() {
+  // Handshake message that we're going to build step-by-step
   std::vector<uint8_t> message;
+  // Reserve some bytes so all calls to `insert` don't need to make heap
+  // allocations, thus making them faster.
   message.reserve(HANDSHAKE_LENGTH);
 
   // Length of protocol identifier
@@ -53,16 +61,20 @@ void Downloader::handshake() {
   // Peer id
   message.insert(message.end(), peerId.begin(), peerId.end());
 
+  // The message is ready, send it to the peer
   socket->write_some(asio::buffer(message));
 
+  // Read the response
   std::array<uint8_t, HANDSHAKE_LENGTH> response{};
   socket->read_some(asio::buffer(response));
 
+  // Extract the info-hash from the response
   hash::hash_t response_info_hash;
   std::copy(response.begin() + INFO_HASH_OFFSET,
             response.begin() + INFO_HASH_OFFSET + sizeof(hash::hash_t{}),
             response_info_hash.begin());
 
+  // In a BitTorrent handshake, the peer should respond with the same info-hash
   if (torrent.info_hash != response_info_hash) {
     throw std::runtime_error("bad handshake");
   }
--- src/download/downloader.hpp
+++ src/download/downloader.hpp
@@ -11,21 +11,33 @@ using namespace fur::peer;
 using namespace fur::torrent;
 
 namespace fur::downloader {
+/// The type used for TCP communication
 using Socket = asio::ip::tcp::socket;
 
+/// Handles downloading of torrent pieces. Must be initialized with a
+/// `TorrentFile` and a `Peer` discovered from that same torrent. This type is
+/// intrinsically not copyable because it embeds an ASIO socket.
 class Downloader {
  public:
+  /// Construct a new `Downloader`. No TCP socket is established at this time.
   explicit Downloader(const TorrentFile& torrent, const Peer& peer);
 
  private:
   const TorrentFile& torrent;
   const Peer& peer;
 
+  /// Socket that this `Downloader` has established with a `Peer`. This is
+  /// lazily initialized when needed and kept in good health thanks to
+  /// `ensure_connected`.
   std::optional<Socket> socket;
 
+  /// Ensures that the `socket` is present and in good health (not dropped,
+  /// timed out and such). Should always call this method first, before
+  /// accessing the socket.
   void ensure_connected();
   void handshake();
 
+  // Befriend this class so the unit tests are able to access private members.
   friend TestingFriend;
 };
 }  // namespace fur::downloader
