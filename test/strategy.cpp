1889ef690a60d9528bb713968b5ccc173aaf3710
Z1ko <filippo.ziche@gmail.com>
2022-09-02 18:24:23 +0200

    Fixed first errors in Furrent test

---
+++ test/strategy.cpp
@@ -0,0 +1,30 @@
+#include "catch2/catch.hpp"
+
+#include <strategy/local.hpp>
+#include <strategy/global.hpp>
+#include <torrent_manager.hpp>
+
+using namespace fur;
+using namespace fur::torrent;
+using namespace fur::strategy;
+
+TEST_CASE("[strategy, global] Round Robin") {
+    // TODO
+}
+
+TEST_CASE("[strategy, global] Smaller First") {
+    // TODO
+}
+
+TEST_CASE("[strategy, local] Random Uniform") {
+
+    //TorrentFile torrent;
+    //TorrentManager manager(torrent);
+    //auto strategy = make_strategy_local<LocalStrategyType::RandomUniform>(manager);
+
+
+}
+
+TEST_CASE("[strategy, local] Streaming") {
+    // TODO
+}
