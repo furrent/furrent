cb62e15723ca2ebc36ee4dde3287354cfd654597
Elia Perantoni <perantonielia0@gmail.com>
2022-09-09 19:11:06 +0200

    Alice faker handle dropped sockets gracefully

--- test/faker/alice_seeder.py
+++ test/faker/alice_seeder.py
@@ -60,6 +60,82 @@ def read_fixture(piece_index, piece_offset, length):
         return f.read(length)
 
 
+def handle(conn):
+    handshake = conn.recv(68)
+
+    try:
+        print(f"Handshake from {handshake[-20:].decode()}")
+    except UnicodeDecodeError:
+        print("Handshake from <non utf8 peer id>")
+
+    # Accept any handshake and reply with some sort of peer id
+    conn.send(handshake[:-20] + b"WhoLetTheDogsOut----")
+
+    bitfield = random_bitfield_fresh()
+    print(f"Bitfield is {bitfield:08b}")
+    # Send a bitfield (there are 5 pieces so that makes it just 1 byte)
+    conn.send(b"\x00\x00\x00\x02\x05" + struct.pack("B", bitfield))
+
+    # Send an unchoke message
+    conn.send(b"\x00\x00\x00\x01\x01")
+
+    # Unchoke
+    assert conn.recv(5) == b"\x00\x00\x00\x01\x01"
+    # Interested
+    assert conn.recv(5) == b"\x00\x00\x00\x01\x02"
+
+    while True:
+        request = conn.recv(17)
+        if not request:
+            # This is an EOF
+            print("EOF")
+            break
+        # The length of a request message
+        assert request[3] == 13
+        # ID of a request message
+        assert request[4] == 6
+
+        # The index of the piece
+        piece_index = struct.unpack(">i", request[5:9])[0]
+
+        # Check that the peer did not dare ask for a piece we don't have
+        assert bitfield_has(bitfield, piece_index)
+
+        # What's the offset from the start of the piece
+        piece_offset = struct.unpack(">i", request[9:13])[0]
+        # How many bytes are requested
+        length = struct.unpack(">i", request[13:])[0]
+
+        payload = read_fixture(piece_index, piece_offset, length)
+
+        # Sometimes send a corrupt block
+        if random.random() < 0.1:
+            payload = bytearray(payload)
+            # Set a random byte to a random value
+            payload[random.randint(0, length - 1)] = random.randint(0, 255)
+            payload = bytes(payload)
+
+        message = bytes()
+        message += struct.pack(">i", 9 + len(payload))
+        message += b"\x07"
+        message += struct.pack(">i", piece_index)
+        message += struct.pack(">i", piece_offset)
+        message += payload
+
+        conn.send(message)
+
+        if not bitfield_complete(bitfield):
+            # Let's say we have acquired a new piece
+            bitfield, new_piece_i = random_bitfield_update(bitfield)
+            print(f"Now also have piece {new_piece_i}")
+            # Send a Have message
+            conn.send(b"\x00\x00\x00\x05\x04" + struct.pack(">i", new_piece_i))
+
+        # Randomly send a KeepAlive, just because we can
+        if random.random() < 0.2:
+            conn.send(b"\x00\x00\x00\x00")
+
+        
 # A simplified BitTorrent client that seeds an "Alice in Wonderland" .txt ISO
 def faker_alice():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,76 +144,7 @@ def faker_alice():
 
     while True:
         conn, _ = sock.accept()
-        handshake = conn.recv(68)
-
         try:
-            print(f"Handshake from {handshake[-20:].decode()}")
-        except UnicodeDecodeError:
-            print("Handshake from <non utf8 peer id>")
-
-        # Accept any handshake and reply with some sort of peer id
-        conn.send(handshake[:-20] + b"WhoLetTheDogsOut----")
-
-        bitfield = random_bitfield_fresh()
-        print(f"Bitfield is {bitfield:08b}")
-        # Send a bitfield (there are 5 pieces so that makes it just 1 byte)
-        conn.send(b"\x00\x00\x00\x02\x05" + struct.pack("B", bitfield))
-
-        # Send an unchoke message
-        conn.send(b"\x00\x00\x00\x01\x01")
-
-        # Unchoke
-        assert conn.recv(5) == b"\x00\x00\x00\x01\x01"
-        # Interested
-        assert conn.recv(5) == b"\x00\x00\x00\x01\x02"
-
-        while True:
-            request = conn.recv(17)
-            if not request:
-                # This is an EOF
-                print("EOF")
-                break
-            # The length of a request message
-            assert request[3] == 13
-            # ID of a request message
-            assert request[4] == 6
-
-            # The index of the piece
-            piece_index = struct.unpack(">i", request[5:9])[0]
-
-            # Check that the peer did not dare ask for a piece we don't have
-            assert bitfield_has(bitfield, piece_index)
-
-            # What's the offset from the start of the piece
-            piece_offset = struct.unpack(">i", request[9:13])[0]
-            # How many bytes are requested
-            length = struct.unpack(">i", request[13:])[0]
-
-            payload = read_fixture(piece_index, piece_offset, length)
-
-            # Sometimes send a corrupt block
-            if random.random() < 0.1:
-                payload = bytearray(payload)
-                # Set a random byte to a random value
-                payload[random.randint(0, length - 1)] = random.randint(0, 255)
-                payload = bytes(payload)
-
-            message = bytes()
-            message += struct.pack(">i", 9 + len(payload))
-            message += b"\x07"
-            message += struct.pack(">i", piece_index)
-            message += struct.pack(">i", piece_offset)
-            message += payload
-
-            conn.send(message)
-
-            if not bitfield_complete(bitfield):
-                # Let's say we have acquired a new piece
-                bitfield, new_piece_i = random_bitfield_update(bitfield)
-                print(f"Now also have piece {new_piece_i}")
-                # Send a Have message
-                conn.send(b"\x00\x00\x00\x05\x04" + struct.pack(">i", new_piece_i))
-
-            # Randomly send a KeepAlive, just because we can
-            if random.random() < 0.2:
-                conn.send(b"\x00\x00\x00\x00")
+            handle(conn)
+        except ConnectionResetError:
+            print("Socket dropped")
