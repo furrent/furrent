8e94fb684cf3661af922304c507f0340940d85bf
Elia Perantoni <perantonielia0@gmail.com>
2022-09-10 19:44:43 +0200

    Sample main program to test out the Downloader

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,58 @@
+#include <cstdint>
 #include <fstream>
+#include <iostream>
 #include <log/logger.hpp>
 #include <sstream>
 
-#include <furrent.hpp>
+#include "bencode/bencode_parser.hpp"
+#include "download/downloader.hpp"
+#include "peer.hpp"
+#include "torrent.hpp"
 
-const char* TORRENT_1 = "../extra/debian-11.4.0-amd64-netinst.iso.torrent";
-const char* TORRENT_2 = "../extra/ubuntu-22.04.1-desktop-amd64.iso.torrent";
+using namespace fur::bencode;
+using namespace fur::download;
+using namespace fur::download::downloader;
 
-int main() {
-
+int main(int argc, char* argv[]) {
   fur::log::initialize_custom_logger();
   auto logger = spdlog::get("custom");
-  
-  // TODO enable only in debug mode
-  logger->set_level(spdlog::level::debug);
 
-  fur::Furrent furrent;
-  furrent.add_torrent(TORRENT_1);
-  furrent.add_torrent(TORRENT_2);
+  if (argc != 4) {
+    logger->error("usage: furrent <path> <ip> <port>");
+    return 1;
+  }
 
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  std::string ip = std::string(argv[2]);
+  uint16_t port = std::atoi(argv[3]);
+
+  Peer peer(ip, port);
+
+  // Read torrent file to string
+  std::ifstream f(argv[1]);
+  std::stringstream buf;
+  buf << f.rdbuf();
+  std::string content = buf.str();
+
+  // Parse bencode
+  BencodeParser parser;
+  auto ben_tree = parser.decode(content);
+
+  // Parse TorrentFile
+  TorrentFile torrent(*ben_tree);
+
+  Downloader down(torrent, peer);
+
+  for (int i = 0; i < static_cast<int>(torrent.piece_hashes.size()); i++) {
+    while (true) {
+      auto maybe_downloaded = down.try_download(Task{i});
+      if (!maybe_downloaded.valid()) continue;
+
+      for (auto c : maybe_downloaded->content) {
+        std::cout << c;
+      }
+      break;
+    }
+  }
+
   return 0;
 }
