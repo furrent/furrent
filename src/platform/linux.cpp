2d571f6a63dd7a3908db74372f3a243e60c077ce
Filippo Ziche <filippo.ziche@gmail.com>
2022-09-13 11:46:38 +0200

    Added platform layer

---
+++ src/platform/linux.cpp
@@ -0,0 +1,47 @@
+#include <platform/io.hpp>
+
+#define _POSIX_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
+#undef _POSIX_SOURCE
+
+namespace fur::platform::io {
+
+IOResult touch(const std::string& filename, size_t size) {
+
+    // Create new file
+    int fd = creat(filename.c_str(), S_IRUSR | S_IWUSR);
+    if (fd < 0)
+        goto error;
+
+    // Set correct size
+    if (ftruncate64(fd, size) < 0)
+        goto error;
+
+    close(fd);
+    return IOResult::OK({}); 
+
+error:
+    close(fd);
+    return IOResult::ERROR(IOError::GenericError);
+}
+
+IOResult write_bytes(const std::string& filename, const std::vector<uint8_t>& bytes, size_t offset) {
+    int fd = open(filename.c_str(), O_CREAT|O_WRONLY);
+    if (fd < 0)
+        goto error;
+
+    if (pwrite(fd, &bytes[0], bytes.size(), offset) < 0)
+        goto error;
+
+    close(fd);
+    return IOResult::OK({});
+
+error:
+    close(fd);
+    return IOResult::ERROR(IOError::GenericError);
+}
+
+} // namespace fur::platform::io
+
+
