4ade5497efa3027401cdbd473a2c61eb3c57db4b
Frex <34170904+nicofretti@users.noreply.github.com>
2022-09-14 16:11:02 +0200

    Init scrollpanel

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #define GUI_FILE_DIALOG_IMPLEMENTATION
 #include "gui/file_dialog.h"
 
+void draw_list_element(std::string text, int pos){
+  GuiDrawText(text.c_str(), Rectangle{10, static_cast<float>(300+pos), 0, 200}, TEXT_ALIGN_LEFT,
+              BLACK);
+}
+
+
 int main() {
   fur::log::initialize_custom_logger();
   auto logger = spdlog::get("custom");
@@ -30,10 +36,8 @@ int main() {
       InitGuiFileDialog(550, 500, "/home/nicof/Desktop/univr/furrent/extra/", false,".torrent");
   file_dialog.fileTypeActive = true;
 
-  //auto x = LoadDirectoryFiles("/home/nicof/Desktop/univr/furrent/extra/");
-  //for (int i = 0; i<x.capacity; i++) {
-  //  logger->info("{}", x.paths[i]);
-  //}
+  std::vector<std::string> torrents{ "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+  Vector2 scroll = {0, 0};
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
@@ -41,14 +45,35 @@ int main() {
     GuiDrawText("Furrent", Rectangle{border, border, 0, 50}, TEXT_ALIGN_LEFT,
                 BLACK);
     auto button_file_dialog =
-        GuiButton(Rectangle{w_width - border - 150, 10, 150, 30},
+        GuiButton(Rectangle{w_width - border - 150, 0, 150, 30},
                   GuiIconText(ICON_FOLDER_OPEN, "Open torrent"));
-
     // Adding button to the right of the group box
-    GuiListView(Rectangle{border, 50, w_width - border * 2, w_height},
-                "Torrent list", &flag_true, flag_true);
 
+    GuiScrollPanel(Rectangle{border, 50, w_width - border * 2, w_height-50},
+                   "Torrents",
+                    Rectangle{border, 50, w_width - 100, 1000},
+                   &scroll);
+
+    int pos = 0;
+    for(auto t: torrents){
+      draw_list_element(t, pos);
+      pos+=100;
+    }
+    GuiDrawRectangle(
+        Rectangle{border, w_height-2, w_width, w_width},
+        1,
+        GetColor(0xf5f5f5ff),
+        GetColor(0xf5f5f5ff));
+
+
+    // ------
     // Events
+    // ------
+    float wheelMove = GetMouseWheelMove();
+    if (wheelMove != 0) {
+      scroll.y += wheelMove * 20;
+    }
+
     // Button add torrent
     if (button_file_dialog) {
       file_dialog.fileDialogActive = true;
@@ -71,3 +96,5 @@ int main() {
 
   return 0;
 }
+
+
