b623e351425cbf16d43ff8c1508f193cda085a18
Elia Perantoni <perantonielia0@gmail.com>
2022-11-14 15:25:48 +0100

    Change progress bar colors

--- src/gui/colors.hpp
+++ src/gui/colors.hpp
@@ -12,9 +12,9 @@ const int ORANGE_HEX = static_cast<int>(0xD27607FF);
 const int LIGHT_BACKGROUND_HEX = static_cast<int>(0xF6F2EEFF);
 const int DARK_BACKGROUND_HEX = static_cast<int>(0xBA9978FF);
 const int PRESSED_BACKGROUND_HEX = static_cast<int>(0xEDE9E0FF);
-const int DOWNLOADING_COLOR_HEX = static_cast<int>(0xC9EFFEFF);
+const int DOWNLOADING_COLOR_HEX = static_cast<int>(0x0091FFFF);
 const int STOP_COLOR_HEX = static_cast<int>(0xFFAF7AFF);
-const int DONE_COLOR_HEX = static_cast<int>(0xABF7B1FF);
+const int DONE_COLOR_HEX = static_cast<int>(0x00FF5EFF);
 const int ERROR_COLOR_HEX = static_cast<int>(0xFFA590FF);
 
 // Raygui colors
